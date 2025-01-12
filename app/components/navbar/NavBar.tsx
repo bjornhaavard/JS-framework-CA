af006f035b8b63629716186bb286a8896c769829
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2025-01-12 16:00:41 +0100

    added a default profile picture

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -86,13 +86,7 @@ export function NavBar() {
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <Image
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt="User Avatar"
-                        width={32}
-                        height={32}
-                        className="h-8 w-8 rounded-full"
-                      />
+                      <Image src="/images/11539820.png" alt="User Avatar" width={32} height={32} className="h-8 w-8 rounded-full" />
                     </Menu.Button>
                   </div>
                   <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
